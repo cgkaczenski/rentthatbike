c9c6431dd0f2518b741cdbcab2d7573e1ecd92e9
cgkaczenski <kzpulaski@gmail.com>
2016-07-25 18:32:17 -0500

    init

---
+++ rentthatbike/scripts/app/app.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    var myAppModule = angular.module(
+        'myApp',
+        [
+            'ngRoute',
+            'ui.bootstrap'
+        ]);
+
+    myAppModule.config([
+        '$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/', { templateUrl: 'scripts/app/views/default.html' })
+                .when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html', controller: 'BicyclesController' })
+                .when('/bicycles/new', { templateUrl: 'scripts/app/views/bicyclesNew.html', controller: 'BicyclesController'})
+                .when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html' })
+                .when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
+        }
+    ]);
+
+})();
