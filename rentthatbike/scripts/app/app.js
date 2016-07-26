91fa13f7e054277f50264559bba6671e61735393
cgkaczenski <kzpulaski@gmail.com>
2016-07-26 01:09:13 -0500

    Add bicycle edit view
    Continuing with chapter 2 of Learning AngularJS for .NET Developers.
    -Refactor bicyclesEditor view to be used by two routes: /new and /edit
    -Debug bootstrap form input for /edit view, cutting off input for class col-md-1

--- rentthatbike/scripts/app/app.js
+++ rentthatbike/scripts/app/app.js
@@ -13,7 +13,8 @@
             $routeProvider
                 .when('/', { templateUrl: 'scripts/app/views/default.html' })
                 .when('/bicycles', { templateUrl: 'scripts/app/views/bicyclesIndex.html', controller: 'BicyclesController' })
-                .when('/bicycles/new', { templateUrl: 'scripts/app/views/bicyclesNew.html', controller: 'BicycleController'})
+                .when('/bicycles/new', { templateUrl: 'scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' })
+                .when('/bicycles/:bicycleId/edit', { templateUrl: 'scripts/app/views/bicyclesEditor.html', controller: 'BicycleController' })
                 .when('/customers', { templateUrl: 'scripts/app/views/customersIndex.html' })
                 .when('/rentals', { templateUrl: 'scripts/app/views/rentalsIndex.html' });
         }
